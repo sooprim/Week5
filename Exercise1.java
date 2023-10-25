dd6740ef8a1e9472e362d5e7d883e2a9776ba878
sooprim <147048730+sooprim@users.noreply.github.com>
2023-10-25 18:41:44 +0200

    Create Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,34 @@
+package week5;
+
+public class Exercise1{
+	public static void main(String[] ars) {
+		String s = "Computer";
+		int length = s.length();
+		
+		char first = s.charAt(0);
+		char middle = s.charAt(s.length() / 2);
+		char last = s.charAt(s.length() - 1);
+		
+		System.out.println("Original: " + s);
+		System.out.println("In small letters: " + s.toLowerCase());
+		System.out.println("In capital letters: " + s.toUpperCase());
+		System.out.println("Length: " + length);
+		
+		if(s.startsWith("Com")){
+			System.out.println("Starts with 'Com': true");
+		}
+		else{
+			System.out.println("Starts with 'Com': false");
+		}
+		
+		if(s.endsWith("ion"))
+		{
+			System.out.println("Ends with 'ion': true");
+		}
+		else{
+			System.out.println("Ends with 'ion': false");
+		}
+		
+		//professor scrolled before i could finish :)
+	}
+}
